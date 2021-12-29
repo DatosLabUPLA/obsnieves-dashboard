3db6dc7e10431ab83bad355ed3442a588c2fe851
Alejandro Espinoza Gallardo <alejandroespinoza1996@gmail.com>
2021-12-29 11:17:02 -0300

    actulización menu y mapas

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,4 +5,8 @@ router.get('/', (req, res) => {
     res.render('index.html', { title: 'Observatorio Satelital de Nieves' });
 });
 
+router.get('/landsat_mensual', (req, res) => {
+    res.render('landsatmensual.html', { title: 'Observatorio Satelital de Nieves' });
+});
+
 module.exports = router;
