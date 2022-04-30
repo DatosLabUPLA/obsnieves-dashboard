5e1b2855d8006a492750245b2f5685176cec4fc1
Alejandro Espinoza Gallardo <alejandroespinoza1996@gmail.com>
2022-04-30 18:09:42 -0400

    Corrección de Errores
    Se hace corrección de nombres en gráficos, se agrega .clip() a mapas y se agregan detalles de interfaz gráfica

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get('/landsat_mensual', (req, res) => {
     res.render('index.html', { title: 'Observatorio Satelital de Nieves' });
 });
 
