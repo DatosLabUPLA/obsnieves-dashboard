7e5c3c1ef0ba37b0cf41d62a9c91ed27ca298719
Alejandro Espinoza Gallardo <alejandroespinoza1996@gmail.com>
2022-04-30 18:14:54 -0400

    Corrección error de ruta

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/landsat_mensual', (req, res) => {
+router.get('/', (req, res) => {
     res.render('index.html', { title: 'Observatorio Satelital de Nieves' });
 });
 
